8b0fd00ee217b07f31690f2f7e63b23d357e7d28
Polina Limonova <polina98_08@mail.ru>
2024-04-01 15:26:55 +0300

    Initialization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
+import { Separator } from "@/components/ui/separator";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="flex min-h-screen w-full flex-col items-center dark:bg-black">
+            <Navbar />
+            <Separator />
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
